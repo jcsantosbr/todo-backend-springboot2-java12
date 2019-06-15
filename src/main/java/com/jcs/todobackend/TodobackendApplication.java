32560468ea11036660377839aafe66790654aafc
jcsantosbr <3025846+jcsantosbr@users.noreply.github.com>
2019-06-15 22:22:58 +0200

    Configure web project

--- src/main/java/com/jcs/todobackend/TodobackendApplication.java
+++ src/main/java/com/jcs/todobackend/TodobackendApplication.java
@@ -1,13 +1,15 @@
 package com.jcs.todobackend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class TodobackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodobackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TodobackendApplication.class, args);
+  }
 
 }

fb7c8e71e5cf604e9c460c377dc1cf374589e0e3
jcsantosbr <3025846+jcsantosbr@users.noreply.github.com>
2019-06-15 20:57:32 +0200

    Add Spring Boot generated skeleton

---
+++ src/main/java/com/jcs/todobackend/TodobackendApplication.java
@@ -0,0 +1,13 @@
+package com.jcs.todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodobackendApplication.class, args);
+	}
+
+}

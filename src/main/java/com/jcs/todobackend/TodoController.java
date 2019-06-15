1ef4457574f5f72a6606434ea5e8f205836f9c74
jcsantosbr <3025846+jcsantosbr@users.noreply.github.com>
2019-06-15 23:55:40 +0200

    Configure CORS and /todos endpoint

--- src/main/java/com/jcs/todobackend/TodoController.java
+++ src/main/java/com/jcs/todobackend/TodoController.java
@@ -1,12 +1,26 @@
 package com.jcs.todobackend;
 
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.OPTIONS;
+import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(
+    methods = {POST, GET, OPTIONS, DELETE, PATCH},
+    maxAge = 3600,
+    allowedHeaders = {"x-requested-with", "origin", "content-type", "accept"},
+    origins = "*"
+)
 @RestController
+@RequestMapping("/todos")
 public class TodoController {
 
-  @RequestMapping("/")
+  @RequestMapping(method = GET)
   public String index() {
     return "ping";
   }
